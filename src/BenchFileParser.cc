ec10996a163938dd7baa96dcb905158ed7fa9882
Sadik Özoguz <sadik@informatik.uni-bremen.de>
2016-10-21 16:14:07 +0200

    initial read_header method, reading inputs, outputs, inverter

--- src/BenchFileParser.cc
+++ src/BenchFileParser.cc
@@ -7,6 +7,7 @@
 #include <boost/spirit/include/classic.hpp>
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/qi_match.hpp>
+#include <boost/lexical_cast.hpp>
 #include <boost/spirit/version.hpp>
 
 
@@ -20,25 +21,84 @@ BenchFileParser::BenchFileParser()
 
 }
 
-void BenchFileParser::parseFile(std::string inputFile)
+unsigned BenchFileParser::count_inputs(std::string inputFile)
 {
-    cout << "[INFO] try to parse " << inputFile.c_str() << endl;
-
+    unsigned input_counter = 0;
     std::ifstream file(inputFile.c_str());
     std::string line;
-    unsigned input_counter = 0;
 
     while (std::getline(file, line))
     {
         cout << "[DEBUG] " << line << endl;
         parse(line.c_str(),
-              *( boost::spirit::classic::str_p("INPUT")[increment_a(input_counter)]
-              | boost::spirit::classic::anychar_p
+              *( str_p("inputs")[increment_a(input_counter)]
+              | anychar_p
               ));
         cout << "[DEBUG] input_counter: " << input_counter << endl;
     }
 
-    cout << "[DEBUG] file contains " << input_counter << " inputs" << endl;
+    return input_counter;
+}
+
+void BenchFileParser::read_header(std::string inputFile)
+{
+    namespace qi = boost::spirit::qi;
+    using boost::spirit::qi::skip;
+
+    std::ifstream file(inputFile.c_str());
+    std::string line;
+
+    while (std::getline(file, line) && boost::starts_with(line, "#"))
+    {
+        cout << "[DEBUG] " << line << endl;
+        std::vector<std::string> counter;
+
+        //inputs section
+        bool const i_result = qi::parse(line.begin(), line.end(),
+                  skip(qi::space) ["#" >> +qi::digit >> "inputs"],
+                  counter);
+
+        if (i_result && counter.size() == 1)
+        {
+            m_inputs = boost::lexical_cast<unsigned>(counter[0]);
+            cout << "    [DEBUG]: " << m_inputs << endl;
+        }
+
+        //outputs section
+        counter.clear();
+        bool const o_result = qi::parse(line.begin(), line.end(),
+                  skip(qi::space) ["#" >> +qi::digit >> "outputs"],
+                  counter);
+
+        if (o_result && counter.size() == 1)
+        {
+            m_outputs = boost::lexical_cast<unsigned>(counter[0]);
+            cout << "    [DEBUG]: " << m_outputs << endl;
+        }
+
+        //inverters section
+        counter.clear();
+        bool const inv_result = qi::parse(line.begin(), line.end(),
+                  skip(qi::space) ["#" >> +qi::digit >> "inverter"],
+                  counter);
+
+        if (inv_result && counter.size() == 1)
+        {
+            m_inverter = boost::lexical_cast<unsigned>(counter[0]);
+            cout << "    [DEBUG]: " << m_inverter << endl;
+        }
+    }
+}
+
+void BenchFileParser::parseFile(std::string inputFile)
+{
+    cout << "[INFO] try to parse " << inputFile.c_str() << endl;
+
+    //unsigned input_counter = count_inputs(inputFile);
+    //cout << "[DEBUG] file contains " << input_counter << " inputs" << endl;
+
+    cout << "[INFO] reading header" << endl;
+    read_header(inputFile);
     cout << "[INFO] end parsing " << inputFile.c_str() << endl;
 }
 
--- src/BenchFileParser.h
+++ src/BenchFileParser.h
@@ -9,5 +9,17 @@ class BenchFileParser
 public:
     BenchFileParser();
 private:
+    unsigned m_inputs;
+    unsigned m_outputs;
+    unsigned m_dff;
+    unsigned m_inverter;
+    unsigned m_ANDs;
+    unsigned m_ORs;
+    unsigned m_NANDs;
+    unsigned m_NORs;
+    unsigned m_buffers;
+private:
     void parseFile(std::string inputFile);
+    unsigned count_inputs(std::string inputFile);
+    void read_header(std::string inputFile);
 };
